daf29232bdd8d5a728e2e0fd5e9dc515a6d67ee2
Tim Morris <TimDMorris@users.noreply.github.com>
2022-07-04 08:10:57 +0100

    ci: add unit test stage to CI pipeline

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ShellComponent } from './shell/shell.component';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -13,7 +13,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, ShellComponent],
+  declarations: [AppComponent, ShellComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
--- apps/demo/src/app/shell/shell.component.spec.ts
+++ apps/demo/src/app/shell/shell.component.spec.ts
@@ -1,11 +1,11 @@
 import { LayoutModule } from '@angular/cdk/layout';
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ShellComponent } from './shell.component';
 
@@ -17,13 +17,13 @@ describe('ShellComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ShellComponent],
       imports: [
-        NoopAnimationsModule,
         LayoutModule,
         MatButtonModule,
         MatIconModule,
         MatListModule,
         MatSidenavModule,
         MatToolbarModule,
+        NoopAnimationsModule,
       ],
     }).compileComponents();
   }));
