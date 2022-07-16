203308112c924326abee814e8debedd200ff5f85
Tim Morris <TimDMorris@users.noreply.github.com>
2022-07-16 14:45:29 +0100

    ci: add commit lint
    Addinf commit lint using angular rules and commit rules documentation

--- apps/demo-e2e/src/integration/app.spec.ts
+++ apps/demo-e2e/src/integration/app.spec.ts
@@ -5,7 +5,7 @@ describe('demo', () => {
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    cy.login('my-email@something.com');
 
     // Function helper example, see `../support/app.po.ts` file
     getGreeting().contains('Welcome demo');
